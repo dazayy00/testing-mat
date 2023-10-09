20e5ecf846fae77cabf52c976c3538562c6ea421
dazayy00 <134337268+dazayy00@users.noreply.github.com>
2023-10-08 23:02:56 -0600

    factorial

---
+++ src/app/factorial/factorial.ts
@@ -0,0 +1,13 @@
+export function factorial(x: number): number {
+  if (x < 0 || x > 15) {
+    return 0;
+  } else if (x === 0) {
+    return 1;
+  } else {
+    let result = 1;
+    for (let i = 1; i <= x; i++) {
+      result *= i;
+    }
+    return result;
+  }
+}
