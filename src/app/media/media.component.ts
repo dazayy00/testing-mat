3888fd59c25475e76d2fb4261e6d5219f00e3fe5
dazayy00 <134337268+dazayy00@users.noreply.github.com>
2023-10-12 09:03:04 -0600

    media

--- src/app/media/media.component.ts
+++ src/app/media/media.component.ts
@@ -1,12 +1,13 @@
 export class MediaComponent {
-    calculateMean(data: number[]): number {
-      if (data.length === 0) {
-        return 0;
-      }
-      
-      const sum = data.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-      const mean = sum / data.length;
-      
-      return mean;
+    mediacomponent(data: number[]): number {
+        if (data.length === 0) {
+            return 0;
+        }
+        
+        const sum = data.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+        const mean = sum / data.length;
+        
+        return mean;
     }
-  }
+}
+
